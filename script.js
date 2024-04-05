90f108711dceb2f90ddf96440c72038668dbfbd2
Prajwal Tiwari <fnafchica2004@gmail.com>
2024-04-06 01:51:47 +0530

    Update script.js
    added eleven stranger things first name function

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ cpd.addEventListener('input', () => {
         errordiv.textContent = '';
     }
 });
+
+const el = document.getElementById('el');
+const input = document.getElementById('first_name');
+input.addEventListener('input', () => {
+    if(input.value === "") {
+        el.textContent = '';
+    }
+    //run loop over whole input value to check if it has any numbers
+    for (let i = 0; i < input.value.length; i++) {
+        if (input.value != "" && isNaN(input.value[i]) === false) {
+            el.textContent = 'Are you 11 from Stranger Things?';
+            el.style.color = 'red';
+            //set small font size
+            el.style.fontSize = '0.7rem';
+            break;
+        } else {
+            el.textContent = '';
+        }
+    }
+});
